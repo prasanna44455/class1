599c9445d2f0152ebe7020c38b140617d5981c34
prasanna44455 <109914499+prasanna44455@users.noreply.github.com>
2023-01-17 21:27:10 +0545

    Add files via upload

---
+++ ecommerce-master/insert.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'config.php';
+
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$address = $_POST["address"];
+$city = $_POST["city"];
+$pin = $_POST["pin"];
+$email = $_POST["email"];
+$pwd = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+
+if($mysqli->query("INSERT INTO users (fname, lname, address, city, pin, email, password) VALUES('$fname', '$lname', '$address', '$city', $pin, '$email', '$pwd')")){
+	echo 'Data inserted';
+	echo '<br/>';
+}
+
+header ("location:login.php");
+
+?>
